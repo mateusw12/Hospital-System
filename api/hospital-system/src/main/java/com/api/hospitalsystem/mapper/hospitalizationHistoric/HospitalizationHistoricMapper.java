a920b7817d319dcb33ecf88f55a52a8c3523b9b0
mateusw12 <38771418+mateusw12@users.noreply.github.com>
2023-03-27 17:41:02 -0300

    Implementa histórico de internação

---
+++ api/hospital-system/src/main/java/com/api/hospitalsystem/mapper/hospitalizationHistoric/HospitalizationHistoricMapper.java
@@ -0,0 +1,38 @@
+package com.api.hospitalsystem.mapper.hospitalizationHistoric;
+
+import com.api.hospitalsystem.dto.hospitalizationHistoric.HospitalizationHistoricDTO;
+import com.api.hospitalsystem.model.hospitalizationHistoric.HospitalizationHistoricModel;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HospitalizationHistoricMapper {
+
+    public HospitalizationHistoricDTO toDTO(HospitalizationHistoricModel hospitalizationHistoricModel) {
+        if (hospitalizationHistoricModel == null) {
+            return null;
+        }
+        return new HospitalizationHistoricDTO(
+                hospitalizationHistoricModel.getId(),
+                hospitalizationHistoricModel.getHospitalizationId(),
+                hospitalizationHistoricModel.getCurrentSector(),
+                hospitalizationHistoricModel.getDays()
+        );
+    }
+
+    public HospitalizationHistoricModel toEntity(HospitalizationHistoricDTO hospitalizationDTO) {
+
+        if (hospitalizationDTO == null) {
+            return null;
+        }
+
+        HospitalizationHistoricModel hospitalizationHistoricModel = new HospitalizationHistoricModel();
+        if (hospitalizationDTO.id() != null) {
+            hospitalizationHistoricModel.setId(hospitalizationDTO.id());
+        }
+        hospitalizationHistoricModel.setHospitalizationId(hospitalizationDTO.hospitalizationId());
+        hospitalizationHistoricModel.setDays(hospitalizationDTO.days());
+        hospitalizationHistoricModel.setCurrentSector(hospitalizationDTO.currentSector());
+        return hospitalizationHistoricModel;
+    }
+
+}
