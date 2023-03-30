6d228623fa8f276498d583778df038bd5c4868cb
mateusw12 <38771418+mateusw12@users.noreply.github.com>
2023-03-30 11:47:06 -0300

    Implementa validação para número de telefone

---
+++ api/hospital-system/src/main/java/com/api/hospitalsystem/utils/decorator/phoneNumber/PhoneNumber.java
@@ -0,0 +1,22 @@
+package com.api.hospitalsystem.utils.decorator.phoneNumber;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ ElementType.FIELD })
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = { PhoneNumberValidator.class })
+public @interface PhoneNumber {
+
+    String message() default "O número de telefone informado é inválido";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+
+}
