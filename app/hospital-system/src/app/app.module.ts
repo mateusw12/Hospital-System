bc0697dbdbd0a497a67690f6d84bb06f1971ca0f
Mateus Walz <mateus.walz@gmail.com>
2023-04-03 17:16:59 -0300

    Implementa configurações gerais (#48)
    * Ajusta html inicial da página
    
    * Implementa estilos globais
    
    * Implementa módulos necessários para a aplicação iniciar
    
    * Ajusta dependencias

--- app/hospital-system/src/app/app.module.ts
+++ app/hospital-system/src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BsModalService } from 'ngx-bootstrap/modal';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [BsModalService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
