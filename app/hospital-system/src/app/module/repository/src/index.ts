f761e266d0620f26cb6f07496b20b5e6aad66cd7
mateusw12 <38771418+mateusw12@users.noreply.github.com>
2023-04-06 16:54:07 -0300

    Ajusta repositório de login

--- app/hospital-system/src/app/module/repository/src/index.ts
+++ app/hospital-system/src/app/module/repository/src/index.ts
@@ -1,1 +1,4 @@
+export * from './hospital/hospital.repository';
+export * from './item/item.repository';
 export * from './login/login.repository';
+export * from './permission/permission.repository';
