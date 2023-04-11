e33b8ad7d6371f1041c3f7cbbf5b62f17f7b0671
mateusw12 <38771418+mateusw12@users.noreply.github.com>
2023-04-11 10:47:57 -0300

    Implementa consulta de usuários por hospital

--- api/hospital-system/src/main/java/com/api/hospitalsystem/repository/user/UserRepository.java
+++ api/hospital-system/src/main/java/com/api/hospitalsystem/repository/user/UserRepository.java
@@ -4,11 +4,15 @@ import com.api.hospitalsystem.model.user.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<UserModel, Long> {
 
     UserModel findByCrm(String crm);
 
     UserModel findByUserName(String userName);
+
+    List<UserModel> findAllByHospitalId(Long hospitalId);
 
 }
