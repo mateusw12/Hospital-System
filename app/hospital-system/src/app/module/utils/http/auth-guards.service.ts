b7c38eaa95e2c875281399fd714bcfebc6b55154
Mateus Walz <mateus.walz@gmail.com>
2023-04-13 20:08:50 -0300

    Ajusta validação do token e redirecionamento quando token inválido (#82)

--- app/hospital-system/src/app/module/utils/http/auth-guards.service.ts
+++ app/hospital-system/src/app/module/utils/http/auth-guards.service.ts
@@ -3,10 +3,10 @@ import {
   ActivatedRouteSnapshot,
   CanActivate,
   CanActivateChild,
-  Router,
   RouterStateSnapshot,
   UrlTree
 } from '@angular/router';
+import { NavigationService } from '@module/service';
 import { Observable } from 'rxjs';
 import { AuthenticationService } from '../services/authentication.service';
 
@@ -15,23 +15,23 @@ export class AuthGuardsService implements CanActivate, CanActivateChild {
 
   constructor(
     private authenticationService: AuthenticationService,
-    private routes: Router
+    private navigationService: NavigationService
   ){}
   canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-    const token = this.authenticationService.getToken();
+    const token = this.authenticationService.isTokenValid();
     if (token) {
       return true;
     }
-    this.routes.navigate(['login']);
+    this.navigationService.navigateToLoginPage();
     return false;
   }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-    const token = this.authenticationService.getToken();
+    const token = this.authenticationService.isTokenValid();
     if (token) {
       return true;
     }
-    this.routes.navigate(['login']);
+    this.navigationService.navigateToLoginPage();
     return false;
   }
 
