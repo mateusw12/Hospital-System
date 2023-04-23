acf1598bd1e008a01e885da47513c3b13643dba2
mateusw12 <38771418+mateusw12@users.noreply.github.com>
2023-04-23 17:07:57 -0300

    Refatora componente de menu

--- app/hospital-system/src/app/module/pages/menu/src/menu.component.ts
+++ app/hospital-system/src/app/module/pages/menu/src/menu.component.ts
@@ -1,19 +1,13 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { Hospital, Item, Login } from '@module/models';
+import { Hospital, Item } from '@module/models';
 import {
   HospitalRepository,
   ItemRepository,
-  LoginRepository,
   PermissionRepository,
+  UserRepository,
 } from '@module/repository';
-import { NavigationService } from '@module/service';
 import { untilDestroyed, untilDestroyedAsync } from '@module/utils/common';
-import {
-  AuthenticationService,
-  ErrorHandler,
-  MenuService,
-} from '@module/utils/services';
+import { ErrorHandler, MenuService } from '@module/utils/services';
 import { forkJoin } from 'rxjs';
 
 @Component({
@@ -24,91 +18,30 @@ import { forkJoin } from 'rxjs';
 export class MenuComponent implements OnInit, OnDestroy {
   menuItems: Item[] = [];
   menuItemsFather: string[] = [];
-  selectedDropdownIndex: number | null = null;
   hospital: Hospital = new Hospital();
   hospitals: Hospital[] = [];
+
+  private userName: string = '';
 
   constructor(
     private permisionRepository: PermissionRepository,
     private itemRepository: ItemRepository,
     private hospitalRepository: HospitalRepository,
     private menuService: MenuService,
-    private loginRepository: LoginRepository,
-    private authenticationService: AuthenticationService,
-    private navigationService: NavigationService,
     private errorHandler: ErrorHandler,
-    private route: Router
+    private userRepository: UserRepository
   ) {}
 
   ngOnInit(): void {
     this.loadData();
   }
 
-  menuItemsFiltered(itemFather: string): Item[] {
-    return this.menuItems.filter(
-      (item) => item.descriptionFather === itemFather
-    );
-  }
-
-  onToggleDropdown(index: number): void {
-    if (this.selectedDropdownIndex === index) {
-      this.selectedDropdownIndex = null;
-    } else {
-      this.selectedDropdownIndex = index;
-    }
-  }
-
-  onHospitalChange(hospitalId: number) {
-    setTimeout(() => {
-      this.menuService.onHospitalChange$(hospitalId);
-    }, 300);
-  }
-
-  onToggleSidebar() {
-    const sidebar = document.getElementById('sidebar');
-    if (!sidebar) return;
-    sidebar.classList.toggle('active');
-  }
-
-  onHomeClick(): void {
-    this.navigationService.navigateToHomePage();
-  }
-
-  onLogoutClick(): void {
-    const login = this.getModel();
-    this.loginRepository
-      .logout(login)
-      .pipe(untilDestroyed(this))
-      .subscribe(
-        () => {
-          this.authenticationService.clearUserToken();
-          this.navigationService.navigateToLoginPage();
-        },
-        (error) => this.handleError(error)
-      );
-  }
-
-  onInformationClick(): void {
-    this.navigationService.navigateToInformationPage();
-  }
-
-  onLicenseClick(): void {
-    this.navigationService.navigateToLicensePage();
-  }
-
-  onNewsClick(): void {
-    this.navigationService.navigateToNewsPage();
-  }
-
-  onNavigateClick(item: Item): void {
-    this.route.navigate([item.path]);
-  }
-
   ngOnDestroy(): void {}
 
   private async loadData(): Promise<void> {
+    await this.getLoggedUser();
     forkJoin([
-      this.permisionRepository.findByUserName('mateus'),
+      this.permisionRepository.findByUserName(this.userName),
       this.itemRepository.findAll(),
       this.hospitalRepository.findAll(),
     ])
@@ -127,6 +60,18 @@ export class MenuComponent implements OnInit, OnDestroy {
       );
   }
 
+  private async getLoggedUser(): Promise<void> {
+    try {
+      const user = await untilDestroyedAsync(
+        this.userRepository.findMe(),
+        this
+      );
+      this.userName = user.userName;
+    } catch (error) {
+      this.handleError(error);
+    }
+  }
+
   private async getActiveHospital(): Promise<void> {
     this.hospital = await untilDestroyedAsync(
       this.menuService.getActiveHospital(),
@@ -141,13 +86,6 @@ export class MenuComponent implements OnInit, OnDestroy {
         return unique;
       }, {})
     );
-  }
-
-  private getModel(): Login {
-    const user = this.authenticationService.getUser();
-    const model = new Login();
-    model.userName = user as string;
-    return model;
   }
 
   private handleError(error: unknown): void {
